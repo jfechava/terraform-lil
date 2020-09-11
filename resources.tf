dca09569985c246d1d2a1609d1dbdb3a427cd29e
John Echavarria <johnfre@yahoo.com>
2020-09-11 12:48:14 -0500

    update files

--- resources.tf
+++ resources.tf
@@ -6,28 +6,3 @@ resource "aws_vpc" "environment-example-two" {
         Name = "terraform-aws-vpc-example-two"
     }
 }
-
-resource "aws_subnet" "subnet1" {
-    vpc_id = aws_vpc.environment-example-two.id
-    cidr_block = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)
-    availability_zone = "us-east-1b"
-}
-
-resource "aws_subnet" "subnet2" {
-    vpc_id = aws_vpc.environment-example-two.id
-    cidr_block = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)
-    availability_zone = "us-east-1c"
-}
-
-resource "aws_security_group" "subnetsecurity" {
-    vpc_id = aws_vpc.environment-example-two.id
-
-    ingress {
-        cidr_blocks = [
-            aws_vpc.environment-example-two.cidr_block
-        ]
-        from_port = 80
-        protocol = "tcp"
-        to_port = 80
-    }
-}
--- subnetworks.tf
+++ subnetworks.tf
@@ -1,14 +1,14 @@
 
 resource "aws_subnet" "subnet1" {
-  cidr_block        = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)
-  vpc_id            = aws_vpc.environment-example-two.id
-  availability_zone = "us-west-2a"
+  vpc_id = aws_vpc.environment-example-two.id
+  cidr_block = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_subnet" "subnet2" {
-  cidr_block        = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)
-  vpc_id            = aws_vpc.environment-example-two.id
-  availability_zone = "us-west-2b"
+  vpc_id = aws_vpc.environment-example-two.id
+  cidr_block = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)
+  availability_zone = "us-east-1c"
 }
 
 resource "aws_security_group" "subnetsecurity" {
@@ -16,11 +16,10 @@ resource "aws_security_group" "subnetsecurity" {
 
   ingress {
     cidr_blocks = [
-      aws_vpc.environment-example-two.cidr_block,
+      aws_vpc.environment-example-two.cidr_block
     ]
-
     from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    protocol = "tcp"
+    to_port = 80
   }
 }
